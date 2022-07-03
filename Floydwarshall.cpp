318abd36c9f14a047705aea12f2b4c8e22615bdc
Priyanshubarthwal <103275278+Priyanshubarthwal@users.noreply.github.com>
2022-07-03 12:41:38 +0530

    Update Floydwarshall.cpp

--- Floydwarshall.cpp
+++ Floydwarshall.cpp
@@ -15,20 +15,20 @@ int main(){
 	cout<<"Enter number of vertices and edges\n";
 	cin>>n>>m;
 	cout<<"Enter edges and their weight\n";
-	for(int i=0;i<m;i++){
+	for( i=0;i<m;i++){
 		int x,y,wt;
 		cin>>x>>y>>wt;
 		dist[x][y]=wt;
 	}
 	for(int k=1;k<=n;k++){
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=n;j++){
+		for( i=1;i<=n;i++){
+			for( j=1;j<=n;j++){
 				dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
 			}
 		}
 	}
-	for(int i=1;i<=n;i++){
-			for(int j=1;j<=n;j++){
+	for( i=1;i<=n;i++){
+			for( j=1;j<=n;j++){
 				if(dist[i][j]==INF){
 					cout<<"I ";
 				}
